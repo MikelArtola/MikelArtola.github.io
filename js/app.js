042061383ebd42bc070730e1c275b275fe1c5a06
MikelArtola <martola021@ikasle.ehu.eus>
2024-12-02 09:19:17 +0100

    push

--- js/app.js
+++ js/app.js
@@ -31,7 +31,46 @@ const showCoffees = () => {
     window.addEventListener("load", function() {
       navigator.serviceWorker
         .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
+        .then(function (registration) {
+          return registration.pushManager
+            .getSubscription()
+            .then(async function (subscription) {
+              if (subscription) {
+                return subscription;
+              }
+              const response = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEB69OPb5fhNPw8BS0ZNQjSiBGQf/j1tzvyn3nz1DEzu12jeGXZ+FZEZweVOlWuCWiLjqF+soyJu631glqdJ4SwA==";
+              const vapidPublicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEB69OPb5fhNPw8BS0ZNQjSiBGQf/j1tzvyn3nz1DEzu12jeGXZ+FZEZweVOlWuCWiLjqF+soyJu631glqdJ4SwA==";//await response.text();
+              const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+              
+              return registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: convertedVapidKey,
+              });
+            });
+        })
+        .then(function (subscription) {
+          console.log("PushManager subscription: ", subscription);
+        })
     })
   }
+
+  document.getElementById("doIt").onclick = function () {
+    const payload = document.getElementById("notification-payload").value;
+    const delay = document.getElementById("notification-delay").value;
+    const ttl = document.getElementById("notification-ttl").value;
+    const options = {
+      TTL: req.body.ttl,
+    };
+  
+    setTimeout(function () {
+      webPush
+        .sendNotification(subscription, payload, options)
+        .then(function () {
+          res.sendStatus(201);
+        })
+        .catch(function (error) {
+          console.log(error);
+          res.sendStatus(500);
+        });
+    }, req.body.delay * 1000);
+  };
--- serviceWorker.js
+++ serviceWorker.js
@@ -29,3 +29,12 @@ self.addEventListener("fetch", fetchEvent => {
       })
     )
 })
+
+self.addEventListener("push", function (event) {
+  const payload = event.data ? event.data.text() : "no payload";
+  event.waitUntil(
+    self.registration.showNotification("ServiceWorker Cookbook", {
+      body: payload,
+    }),
+  );
+});
